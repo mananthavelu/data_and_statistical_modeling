2fc90ec5772b0b323cd58a25921db2fc170845cb
Marimuthu Ananthavelu <mari.a.muthu@gmail.com>
2025-03-30 01:47:34 +0100

    Clean up the repository

--- 14_transformers.py
+++ 14_transformers.py
@@ -69,3 +69,29 @@ class SelfAttention(nn.Module):
 
 
 
+# Perceptron as Logical Operators
+# AND operator
+
+# Import the necessary libraries
+import pandas as pd
+
+# Define the inputs, expected outputs from Perceptron
+inputs = [(0,0), (1,0), (0,1), (1,1)]
+expected_outputs = [False, False, False, True]
+
+
+# Guess the weights
+weight_1 = 1
+weight_2 = 1
+bias = 0
+
+actual_outputs = []
+for input, expected_outputs in zip(inputs, expected_outputs):
+    linear_combination = input[0]*weight_1 + input[1]*weight_2 + bias
+    if linear_combination < 0:
+        result = False
+    else:
+        result = True
+    actual_outputs.append(result)
+
+print(actual_outputs)
